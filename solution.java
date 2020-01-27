3cd9a8a771743096d4d85710dfcf0d226010e259
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2020-01-27 20:37:45 +0200

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the birthday function below.
+    static int birthday(List<Integer> s, int d, int m) {
+        int ways = 0;
+        int sum = 0;
+        //Find if there is a way to break the chocolate at all
+        if(m <= s.size()) 
+            for(int i = 0; i < m; i++)
+                sum += s.get(i);
+        if(sum == d) ways++;
+        ///////////////////////////////////////////////////////
+            
+        //Check other possible ways to break it by using a sliding window
+        for(int i = 0; i < s.size()-m; i++)
+        {
+            sum = sum - s.get(i) + s.get(i+m);
+            if(sum == d) ways++;
+        }
+        return ways;
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] sItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> s = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int sItem = Integer.parseInt(sItems[i]);
+            s.add(sItem);
+        }
+
+        String[] dm = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(dm[0]);
+
+        int m = Integer.parseInt(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
